bdafba6520d9c1d9d82ec2e48186e1969c51a107
Mael castellan <115943167+Mael-Cas@users.noreply.github.com>
2024-06-28 22:38:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,14 +8,7 @@ const PORT = process.env.PORT || 10003;
 const allowedIP = '82.65.82.204';
 
 // Middleware pour vérifier l'adresse IP
-app.use((req, res, next) => {
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    if (ip !== allowedIP) {
-        res.status(403).send('Accès refusé');
-    } else {
-        next();
-    }
-});
+
 
 // Servir les fichiers statiques dans le dossier "public"
 app.use(express.static(path.join(__dirname, 'public')));
