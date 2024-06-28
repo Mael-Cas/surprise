3ea8ea30e97c5b22e060d2ef9458f26cecac3428
Mael castellan <115943167+Mael-Cas@users.noreply.github.com>
2024-06-28 12:19:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10003;
 
 // Servir les fichiers statiques dans le dossier "public"
 app.use(express.static(path.join(__dirname, 'public')));
